9fbcf658cae862067f7a3d519d71ed4cf7e121bb
gptestudent <gptestudent@gptestudent.com>
2017-04-29 11:47:19 -0400

    fixed

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var Botkit = require('botkit');
 //const config = require('./config');
 
-const SLACK_BOT_TOKEN = process.env.SLACK_BOT_TOKEN ;
+//const SLACK_BOT_TOKEN = process.env.SLACK_BOT_TOKEN || config.SLACK_BOT_TOKEN_1;
+
 
 var infinispan = require('infinispan');
 var jdgHost = process.env.DATAGRID_HOTROD_SERVICE_HOST || "127.0.0.1";
@@ -46,7 +47,7 @@ var controller = Botkit.slackbot({
 });
 
 var bot = controller.spawn({
-    token: SLACK_BOT_TOKEN
+    token: "xoxb-175184966880-3aq9WGow0DtDPEc7NfTlkZT4"
 })
 
 bot.startRTM(function(err, bot, payload) {
@@ -63,22 +64,28 @@ controller.hears(['why did the chicken cross the road'], 'direct_message,direct_
 controller.hears(['help'], 'direct_message,direct_mention,mention', (bot, message) => {
     bot.reply(message, {
         text: `You can ask me things like:
-    "What is my employee no?"
-    "Do I have any pending time reports?"
+    "What is my employee number ?"
+    "Do I have any pending time reports ?"
     "How many paid vacations do I still have?"
     "Will i get a promotion this year?"`
     });
 });
 
 controller.hears(['What is my employee number ?'], 'direct_message,direct_mention,mention', function(bot, message) {
-    bot.reply(message, "Your Employee No is "+lookup_cache(message.user));
-    console.log("Cahce:"+lookup_cache());
+     console.log("Cahce:"+lookup_cache());
+    var emp = lookup_cache();
+    console.log("Cahce:"+emp);
+     var emp = JSON.parse(lookup_cache());
+    console.log("Cahce:"+emp);
+    bot.reply(message, "Your Employee No is "+emp.employeeNo);
+    console.log("message.user:"+emp.employeeNo);
+    //console.log("Cahce:"+lookup_cache());
     // console.log("bot:"+JSON.stringify(bot));
-    console.log("message:"+JSON.stringify(message));
-     console.log("message.user:"+message.user);
+    console.log("message:"+JSON.stringify(emp));
+     
 });
 
-controller.hears(['Do I have any pending time reports?'], 'direct_message,direct_mention,mention', function(bot, message) {
+controller.hears(['Do I have any pending time reports ?'], 'direct_message,direct_mention,mention', function(bot, message) {
     bot.reply(message, "Good Job, You have no pending time reports"+lookup_cache());
 
 });
@@ -91,81 +98,3 @@ controller.hears(['Please change my salary account detais. My bank routing no is
 });
 
 
-
-//controller.hears(['Change my salary bank account'], 'direct_message,direct_mention,mention', function(bot, message) {
-//    // start a conversation to handle this response. 
-//    bot.startConversation(message, function(err, convo) {
-//
-//        convo.say('Sure, I need few details. Lets get started!');
-//        convo.ask('Shall we proceed Say YES, NO or DONE to quit.'
-//        convo.say('Cool, you said: ' + response.text);
-//        
-//        convo.ask('Shall we proceed Say YES, NO or DONE to quit.', [{
-//                pattern: 'done',
-//                callback: function(response, convo) {
-//                    convo.say('OK you are done!');
-//                    convo.next();
-//                }
-//            },
-//            {
-//                
-//                pattern: bot.utterances.yes,
-//                callback: function(response, convo) {
-//                    convo.say('Super!');
-//                    convo.ask('What is the banks routing no and your account no?');
-//                    console.log("****ask:bot.utterances:" + JSON.stringify(bot.utterances));
-//                    console.log("****:response:" + JSON.stringify(response));
-//                    
-//                    
-//                    convo.say('*****');
-//                    
-//                    convo.next();
-//
-//                }
-//            },
-//            {
-//                pattern: bot.utterances.no,
-//                callback: function(response, convo) {
-//                    convo.say('Perhaps later.');
-//                    // do something else... 
-//                    convo.next();
-//                }
-//            },
-//            {
-//                default: true,
-//                callback: function(response, convo) {
-//                    // just repeat the question 
-//                    convo.repeat();
-//                    convo.next();
-//                }
-//            }
-//        ]);
-//
-//
-//
-//    });
-//    
-//     let askbankDetails = (response, convo) => {
-//
-//        convo.ask('Enter a description for the case', (response, convo) => {
-//            console.log("###convo");
-//            let responses = convo.getResponsesAsArray();
-//            salesforce.createCase({subject: responses[0].answer, description: responses[1].answer})
-//                .then(_case => {
-//                    console.log(_case);
-//                    bot.reply(message, {
-//                        text: "I created the case:",
-//                        attachments: formatter.formatCase(_case)
-//                    });
-//                    convo.next();
-//                })
-//                .catch(error => {
-//                    bot.reply(message, error);
-//                    convo.next();
-//                });
-//        });
-//
-//    };
-//
-//
-//});
